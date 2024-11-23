301cf5300c95261a5b20ee86512538560d68d21e
pierrebataille99 <pierre.bataille99@gmail.com>
2024-11-23 15:35:44 +0100

    MAJ TP2

--- remplissage.py
+++ remplissage.py
@@ -1,3 +1,6 @@
+############################################################# Code de Pierre BATAILLE #################################################
+# TP1 IOT
+
 import sqlite3
 import random
 from datetime import datetime, timedelta
@@ -6,10 +9,11 @@ from datetime import datetime, timedelta
 # Generation de mesures random pour les 4 capteurs
 def inserer_mesures(conn, c):
     capteur_ids = {
-        1: (-10, 100),  # Température : plage -10 à 100 °c
+        1: (-10, 100),  # Tempereture : plage -10 à 100 °c
         2: (0, 100),    # Consommation électrique : plage 0 à 100 KW
         3: (0, 10000),  # Luminosité : plage 0 à 10000 lumens
-        4: (0, 500)     # Niveau d'eau : plage 0 à 500 L
+        4: (0, 500),     # Niveau d'eau : plage 0 à 500 L
+        5: (-10, 100)  # Tempereture : plage -10 à 100 °c
     }
     for _ in range(5):  # Ajouter exactement 5 mesures repatries aléatoirement entre les capteurs
         capteur_id = random.choice(list(capteur_ids.keys()))
@@ -30,7 +34,7 @@ def inserer_mesures(conn, c):
 
 # Génération de factures aleat pour le log01
 def inserer_factures(conn, c, logement_id):
-    types_factures = ['elec', 'Eau', 'Dechets']
+    types_factures = ['Elec', 'Eau', 'Dechets']
     for _ in range(5):  # Ajouter 5 factures
         type_facture = random.choice(types_factures)
         date_facture = datetime.now() 
@@ -42,6 +46,29 @@ def inserer_factures(conn, c, logement_id):
         """, (logement_id, type_facture, date_facture, montant, valeur_consomme))
 
 
+
+
+
+
+
+def inserer_capteurs(conn, c, piece_id):
+    """
+    Insère les 4 types de capteurs (Température, Luminosité, Consommation électrique, Consommation d'eau)
+    pour une pièce spécifique identifiée par piece_id.
+    """
+    capteurs = [
+        {"TYPE_ID": 1, "reference_commerciale": "Capteur Température"},
+        {"TYPE_ID": 2, "reference_commerciale": "Capteur Luminosité"},
+        {"TYPE_ID": 3, "reference_commerciale": "Compteur Électrique"},
+        {"TYPE_ID": 4, "reference_commerciale": "Compteur Eau"}
+    ]
+    
+    for capteur in capteurs:
+        c.execute("""
+            INSERT INTO Capteur_Actionneur (TYPE_ID, reference_commerciale, PIECE_ID)
+            VALUES (?, ?, ?)
+        """, (capteur["TYPE_ID"], capteur["reference_commerciale"], piece_id))
+ 
 
 
 
