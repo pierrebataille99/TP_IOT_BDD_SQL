a6b0d3fee0dd5bfc71b61d99238c2684569e7cac
pierrebataille99 <pierre.bataille99@gmail.com>
2024-11-28 12:47:11 +0100

    MAJ TP3

--- static/scripts.js
+++ static/scripts.js
@@ -1,6 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Site chargé avec succès!');
+
+    const dropdownToggle = document.querySelector('.dropdown-toggle');
+    const dropdownMenu = document.querySelector('.dropdown-menu');
+
+    if (dropdownToggle && dropdownMenu) {
+        dropdownToggle.addEventListener('click', (event) => {
+            event.preventDefault();
+            // Afficher ou masquer le menu
+            dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
+        });
+    } else {
+        console.error('Menu déroulant introuvable.');
+    }
 });
-
-
-
